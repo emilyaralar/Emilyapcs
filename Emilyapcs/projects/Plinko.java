be968c30d20eac3ec4dde32c4b9c2120ea1581a5
Emily Aralar <Arala.Emil2019@ssusd.org>
2018-03-05 07:36:13 -0800

    adds Plinko

---
+++ Emilyapcs/projects/Plinko.java
@@ -0,0 +1,20 @@
+class Plinko {
+     {
+   public static void main(String args[]) 
+    {System.out.print("Enter a postion 1-9: ");
+Scanner scan = new Scanner(System.in);
+
+
+if (position == 0) {
+    ++1
+}
+if (position ==16) {
+    --1
+}
+else if (postition  )
+
+
+
+
+
+}
