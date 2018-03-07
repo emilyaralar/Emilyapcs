9e665f1cbaabe79662eb027826579f53cc290283
Emily Aralar <Arala.Emil2019@ssusd.org>
2018-03-07 08:04:43 -0800

    update plinko

--- Emilyapcs/projects/Plinko.java
+++ Emilyapcs/projects/Plinko.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+
 class Plinko {
-     {
-   public static void main(String args[]) 
-    {System.out.print("Enter a postion 1-9: ");
-Scanner scan = new Scanner(System.in);
+    public static final int SINGLE_DISC = 1;
+    public static final int MULTI_DISC = 2;
+    public static final int TERMINATE = 3;
+   
+    public static final int[] VALUES = {1, 3, 2, 0, 5, 0, 2, 3, 1};
 
+    public static int mode = -1;
 
-if (position == 0) {
-    ++1
-}
-if (position ==16) {
-    --1
-}
-else if (postition  )
-
-
-
-
-
+    public static void main(String[] args) {
+           Scanner scan;
+        while(true) {
+            //Loop to select mode.
+            //This loop is infinite until the user selects the "Quit" option (3)
+            scan = new Scanner(System.in);
+           System.out.println("Select a mode");
+            if(scan.hasNextInt()) {
+                mode = scan.nextInt();
+                if(mode == SINGLE_DISC) {
+                    System.out.println("Mode not yet implemented");
+                }
+                else if(mode == MULTI_DISC) {
+                    System.out.println("Mode not yet implemented");
+                }
+                else if(mode == TERMINATE) {
+                    System.out.println("Goodbye");
+                    break;
+                } else {
+                    continue;
+                }
+            }
+        
+        }   
+    }
 }
